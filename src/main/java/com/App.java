1058e0db0ddb907b77c6712c1cdfb7e9c742e7cf
harit-p <haritpansuriya72@gmail.com>
2024-06-01 14:59:19 +0530

    spring.xml singleton

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,5 +1,7 @@
 package com;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.bean.StudentBean;
 /**
  * Hello world!
  *
@@ -8,6 +10,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+//    	StudentBean s1 = new StudentBean();
+//    	StudentBean s2 = new StudentBean();
+//    	
+//    	System.out.println(s1);
+//    	System.out.println(s2);
+    	
+    	//ios container >
+    	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+    	System.out.println(context);
+    	StudentBean s1 = context.getBean("studentBean",StudentBean.class);
+    	StudentBean s2 = context.getBean("studentBean",StudentBean.class);
+    	
+    	System.out.println(s1);
+    	System.out.println(s2);
+    	
+    	//        System.out.println( "Hello World!" );
     }
 }
